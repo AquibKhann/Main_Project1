9f33cebba5fd407709f4f4ed4b6d981622191772
Aquib Khan <aquibkhanstud@gmail.com>
2024-09-13 00:03:53 +0530

    added packages for authentication and created user model

--- models/review.js
+++ models/review.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { schema } = require("./listing");
+// const { schema } = require("./listing");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
